32e44c7fd89f92a354131fba9eb8a84f9ec27f02
joseX2R <jlrepresa@gmail.com>
2014-05-15 10:46:48 +0200

    Travis tests

---
+++ test/SlotMachine_test.js
@@ -0,0 +1,28 @@
+(function($) {
+  /*
+    ======== A Handy Little QUnit Reference ========
+    http://api.qunitjs.com/
+
+    Test methods:
+      module(name, {[setup][ ,teardown]})
+      test(name, callback)
+      expect(numberOfAssertions)
+      stop(increment)
+      start(decrement)
+    Test assertions:
+      ok(value, [message])
+      equal(actual, expected, [message])
+      notEqual(actual, expected, [message])
+      deepEqual(actual, expected, [message])
+      notDeepEqual(actual, expected, [message])
+      strictEqual(actual, expected, [message])
+      notStrictEqual(actual, expected, [message])
+      throws(block, [expected], [message])
+  */
+
+  test('example test', function() {
+    expect(1);
+    strictEqual('test', 'test', 'test === test');
+  });
+  
+}(jQuery));
