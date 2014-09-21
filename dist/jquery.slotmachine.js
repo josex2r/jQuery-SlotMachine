da200648e0626f143d4bff4e8a662534920a6d65
joseX2R <jlrepresa@gmail.com>
2014-09-21 20:59:40 +0200

    Add bower

--- dist/jquery.slotmachine.js
+++ dist/jquery.slotmachine.js
@@ -1,4 +1,4 @@
-/*! SlotMachine - v2.0.2 - 2014-07-31
+/*! SlotMachine - v2.0.3 - 2014-09-21
 * https://github.com/josex2r/jQuery-SlotMachine
 * Copyright (c) 2014 Jose Luis Represa; Licensed MIT */
 ;(function($, window, document, undefined){
@@ -9,15 +9,21 @@
 			delay	: 200, //Animation time [int]
 			auto	: false, //Repeat delay [false||int]
 			randomize : null, //Randomize function, must return an integer with the selected position
-			stopHidden : true
+			stopHidden : true //Stops animations if the element isn´t on the screen
 		};
+	
+	var FX_FAST = 'slotMachineBlurFast',
+		FX_NORMAL = 'slotMachineBlurMedium',
+		FX_SLOW = 'slotMachineBlurSlow',
+		FX_GRADIENT = 'slotMachineGradient',
+		FX_STOP = FX_GRADIENT;
 			
 	//Set required styles, filters and masks
 	$(document).ready(function(){
 		
 		//Fast blur
-		if( $("filter#slotMachineBlurSVG").length<=0 ){
-			$("body").append('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="0" height="0">'+
+		if( $('filter#slotMachineBlurSVG').length<=0 ){
+			$('body').append('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="0" height="0">'+
 								'<filter id="slotMachineBlurFilterFast">'+
 									'<feGaussianBlur stdDeviation="5" />'+
 								'</filter>'+
@@ -25,8 +31,8 @@
 		}
 		
 		//Medium blur
-		if( $("filter#slotMachineBlurSVG").length<=0 ){
-			$("body").append('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="0" height="0">'+
+		if( $('filter#slotMachineBlurSVG').length<=0 ){
+			$('body').append('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="0" height="0">'+
 								'<filter id="slotMachineBlurFilterMedium">'+
 									'<feGaussianBlur stdDeviation="3" />'+
 								'</filter>'+
@@ -34,8 +40,8 @@
 		}
 		
 		//Slow blur
-		if( $("filter#slotMachineBlurSVG").length<=0 ){
-			$("body").append('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="0" height="0">'+
+		if( $('filter#slotMachineBlurSVG').length<=0 ){
+			$('body').append('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="0" height="0">'+
 								'<filter id="slotMachineBlurFilterSlow">'+
 									'<feGaussianBlur stdDeviation="1" />'+
 								'</filter>'+
@@ -43,8 +49,8 @@
 		}
 		
 		//Fade mask
-		if( $("mask#slotMachineFadeSVG").length<=0 ){
-			$("body").append('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="0" height="0">'+
+		if( $('mask#slotMachineFadeSVG').length<=0 ){
+			$('body').append('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="0" height="0">'+
 								'<mask id="slotMachineFadeMask" maskUnits="objectBoundingBox" maskContentUnits="objectBoundingBox">'+
 									'<linearGradient id="slotMachineFadeGradient" gradientUnits="objectBoundingBox" x="0" y="0">'+
 										'<stop stop-color="white" stop-opacity="0" offset="0"></stop>'+
@@ -58,15 +64,15 @@
 		}
 		
 		//CSS classes
-		$("body").append("<style>"+
-								".slotMachineBlurFast{-webkit-filter: blur(5px);-moz-filter: blur(5px);-o-filter: blur(5px);-ms-filter: blur(5px);filter: blur(5px);filter: url(#slotMachineBlurFilterFast);filter:progid:DXImageTransform.Microsoft.Blur(PixelRadius='5')}"+
-								".slotMachineBlurMedium{-webkit-filter: blur(3px);-moz-filter: blur(3px);-o-filter: blur(3px);-ms-filter: blur(3px);filter: blur(3px);filter: url(#slotMachineBlurFilterMedium);filter:progid:DXImageTransform.Microsoft.Blur(PixelRadius='3')}"+
-								".slotMachineBlurSlow{-webkit-filter: blur(1px);-moz-filter: blur(1px);-o-filter: blur(1px);-ms-filter: blur(1px);filter: blur(1px);filter: url(#slotMachineBlurFilterSlow);filter:progid:DXImageTransform.Microsoft.Blur(PixelRadius='1')}"+
-								".slotMachineGradient{"+
-									"-webkit-mask-image: -webkit-gradient(linear, left top, left bottom, color-stop(0%, rgba(0,0,0,0)), color-stop(25%, rgba(0,0,0,1)), color-stop(75%, rgba(0,0,0,1)), color-stop(100%, rgba(0,0,0,0)) );"+
-									"mask: url(#slotMachineFadeMask);"+
-								"}"+
-							"</style>");
+		$('body').append('<style>' +
+								'.' + FX_FAST + '{-webkit-filter: blur(5px);-moz-filter: blur(5px);-o-filter: blur(5px);-ms-filter: blur(5px);filter: blur(5px);filter: url(#slotMachineBlurFilterFast);filter:progid:DXImageTransform.Microsoft.Blur(PixelRadius="5")}' +
+								'.' + FX_NORMAL + '{-webkit-filter: blur(3px);-moz-filter: blur(3px);-o-filter: blur(3px);-ms-filter: blur(3px);filter: blur(3px);filter: url(#slotMachineBlurFilterMedium);filter:progid:DXImageTransform.Microsoft.Blur(PixelRadius="3")}' +
+								'.' + FX_SLOW + '{-webkit-filter: blur(1px);-moz-filter: blur(1px);-o-filter: blur(1px);-ms-filter: blur(1px);filter: blur(1px);filter: url(#slotMachineBlurFilterSlow);filter:progid:DXImageTransform.Microsoft.Blur(PixelRadius="1")}' +
+								'.' + FX_GRADIENT + '{' +
+									'-webkit-mask-image: -webkit-gradient(linear, left top, left bottom, color-stop(0%, rgba(0,0,0,0)), color-stop(25%, rgba(0,0,0,1)), color-stop(75%, rgba(0,0,0,1)), color-stop(100%, rgba(0,0,0,0)) );' +
+									'mask: url(#slotMachineFadeMask);' +
+								'}'+
+							'</style>');
 		
 	});
 	
@@ -93,7 +99,7 @@
 	  * @param object settings - Plugin configuration params
 	  * @return jQuery node - Returns jQuery selector with some new functions (shuffle, stop, next, auto, active)
 	*/
-	function Plugin(element, options){
+	function SlotMachine(element, options){
 		this.element = element;
 		this.settings = $.extend( {}, defaults, options);
 		this._defaults = defaults;
@@ -101,19 +107,19 @@
 		
 		var	self = this,
 			$slot = $(element), //jQuery selector
-			$titles = $slot.children(), //Slot Machine elements
-			$container, //Container to wrap $titles
+			$tiles = $slot.children(), //Slot Machine elements
+			$container, //Container to wrap $tiles
 			_minTop, //Min marginTop offset
 			_maxTop, //Max marginTop offset
-			_$fakeFirstTitle, //First element (the last of the html container)
-			_$fakeLastTitle, //Last element (the first of the html container)
+			_$fakeFirstTile, //First element (the last of the html container)
+			_$fakeLastTile, //Last element (the first of the html container)
 			_timer = null, //Timeout recursive function to handle auto (settings.auto)
 			_forceStop = false, //Force execution stop for some functions
 			_oncompleteShuffling = null, //Callback function
 			_isRunning = false, //Machine is running?
 			_active = { //Current active element
 				index	: this.settings.active,
-				el		: $titles.get( this.settings.active )
+				el		: $tiles.get( this.settings.active )
 			};
 		
 		/**
@@ -124,17 +130,17 @@
 		function _getOffset( index ){
 			var offset = 0;
 			for(var i=0; i<index; i++){
-				offset += $( $titles.get(i) ).outerHeight();
+				offset += $( $tiles.get(i) ).outerHeight();
 			}
 			return -offset + _minTop;
 		}
 		
 		/**
 		  * @desc PRIVATE - Get current element index
-		  * @return int - $titles element index
+		  * @return int - $tiles element index
 		*/
 		function _getIndexFromOffset(){
-			return Math.abs( Math.round( parseInt( $container.css('margin-top').replace(/px/, ''), 10) / $titles.first().height() ) ) -1;
+			return Math.abs( Math.round( parseInt( $container.css('margin-top').replace(/px/, ''), 10) / $tiles.first().height() ) ) -1;
 		}
 		
 		/**
@@ -146,13 +152,13 @@
 			var rnd,
 				removePrevious = cantBeTheCurrent || false;
 			do{
-				rnd = Math.floor( Math.random() * $titles.length );
+				rnd = Math.floor( Math.random() * $tiles.length );
 			}while( (removePrevious && rnd===_active.index) && rnd>=0 );
 			
 			//Choose element
 			return {
 				index : rnd,
-				el : $titles.get( rnd )
+				el : $tiles.get( rnd )
 			};
 		}
 		
@@ -179,12 +185,12 @@
 			var choosen;
 			if( self.settings.randomize!==null && typeof self.settings.randomize==='function' ){
 				var index = self.settings.randomize(_active.index);
-				if( index<0 || index>=$titles.length ){
+				if( index<0 || index>=$tiles.length ){
 					index = 0;
 				}
 				choosen = {
 					index : index,
-					el : $titles.get( index )
+					el : $tiles.get( index )
 				};
 			}else{
 				choosen = _getRandom();
@@ -197,11 +203,11 @@
 		  * @return int - Element index and HTML node
 		*/ 
 		function _getPrev(){
-			var prevIndex = _active.index-1<0 ? $titles.length-1 : _active.index-1;
+			var prevIndex = _active.index-1<0 ? $tiles.length-1 : _active.index-1;
 			
 			return {
 				index	: prevIndex,
-				el		: $titles.get(prevIndex)
+				el		: $tiles.get(prevIndex)
 			};
 		}
 		
@@ -210,37 +216,26 @@
 		  * @return int - Element index and HTML node
 		*/ 
 		function _getNext(){
-			var nextIndex = _active.index+1<$titles.length ? _active.index+1 : 0;
+			var nextIndex = _active.index+1<$tiles.length ? _active.index+1 : 0;
 			
 			return {
 				index	: nextIndex,
-				el		: $titles.get(nextIndex)
+				el		: $tiles.get(nextIndex)
 			};
 		}
 		
 		/**
 		  * @desc PRIVATE - Set CSS classes to make speed effect
-		  * @param string speed - Element speed [fast||medium||slow]
+		  * @param string FX_SPEED - Element speed [FX_FAST_BLUR||FX_NORMAL_BLUR||FX_SLOW_BLUR||FX_STOP]
 		  * @param string||boolean fade - Set fade gradient effect
 		*/
-		function _setAnimationFX(speed, fade){
-			$titles.removeClass("slotMachineBlurFast slotMachineBlurMedium slotMachineBlurSlow");
-			switch( speed ){
-				case 'fast':
-					$titles.addClass("slotMachineBlurFast");
-					break;
-				case 'medium':
-					$titles.addClass("slotMachineBlurMedium");
-					break;
-				case 'slow':
-					$titles.addClass("slotMachineBlurSlow");
-					break;
-			}
+		function _setAnimationFX(FX_SPEED, fade){
+			$tiles.removeClass(FX_FAST).removeClass(FX_NORMAL).removeClass(FX_SLOW).addClass(FX_SPEED);
 			
-			if( fade!==true || speed==="stop" ){
-				$slot.add($titles).removeClass("slotMachineGradient");
+			if(fade !== true || FX_SPEED === FX_STOP){
+				$slot.add($tiles).removeClass(FX_GRADIENT);
 			}else{
-				$slot.add($titles).addClass("slotMachineGradient");
+				$slot.add($tiles).addClass(FX_GRADIENT);
 			}
 		}
 		
@@ -257,15 +252,16 @@
 		*/
 		function _shuffle( repeations ){
 			
-			if( !_isVisible() ){
+			if(!_isVisible()){
 				
-				_setAnimationFX("stop");
+				_setAnimationFX(FX_STOP);
 				
 				_resetPosition();
 				
 				setTimeout(function(){
 					_stop();
 				}, self.settings.delay);
+				
 				return;
 			}
 			
@@ -274,10 +270,10 @@
 			var delay = self.settings.delay;
 			
 			//Infinite animation
-			if( typeof repeations!=='number' ){
+			if(typeof repeations !== 'number'){
 				
 				//Set animation effects
-				_setAnimationFX("fast", true);
+				_setAnimationFX(FX_FAST, true);
 				
 				delay /= 2;
 				
@@ -286,30 +282,29 @@
 					marginTop : _maxTop
 				}, delay, 'linear', function(){
 					//Oncomplete animation
-					if( _forceStop===false ){
+					if(_forceStop === false){
 						//Reset top position
-						$container.css("margin-top", 0);
+						$container.css('margin-top', 0);
 						//Repeat animation
 						_shuffle();
 					}
 				});
 			
 			//Stop animation after {repeations} repeats
-			}else if( typeof repeations==='number' && repeations>0 ){
+			}else if(typeof repeations === 'number' && repeations > 0){
 				
 				//Change delay and speed
 				switch( repeations ){
 					case 1:
-					case 2:
-						_setAnimationFX("slow", true);
+						_setAnimationFX(FX_SLOW, true);
 						break;
+					case 2:
 					case 3:
-					case 4:
-						_setAnimationFX("medium", true);
+					_setAnimationFX(FX_NORMAL, true);
 						delay /= 1.5;
 						break;
 					default:
-						_setAnimationFX("fast", true);
+						_setAnimationFX(FX_FAST, true);
 						delay /= 2;
 				}
 					
@@ -319,7 +314,7 @@
 				}, delay, 'linear', function(){
 					if( _forceStop===false ){
 						//Reset top position
-						$container.css("margin-top", 0);
+						$container.css('margin-top', 0);
 						//Repeat animation on complete
 						_shuffle( repeations-1 );
 					}
@@ -337,15 +332,17 @@
 		*/
 		function _stop( getElementFn ){
 			//Stop animation NOW!!!!!!!
-			$container.clearQueue().stop(true, false);
-			_setAnimationFX("slow", true);
+			$container.clearQueue().stop();
+			
+			_setAnimationFX(FX_SLOW, true);
+			
 			_isRunning = true;
 			
-			if( !_isVisible() ){
-				if( typeof _oncompleteShuffling==='function' ){
+			if(!_isVisible()){
+				if(typeof _oncompleteShuffling === 'function'){
 					_oncompleteShuffling(false, false);
 				}
-				_setAnimationFX("stop", false);
+				_setAnimationFX(FX_STOP, false);
 				_resetPosition();
 				_isRunning = false;
 				return;
@@ -353,12 +350,12 @@
 			
 			//Get random or custom element
 			var rnd = _getRandom();
-			if( typeof getElementFn==="function" ){
+			if(typeof getElementFn === 'function'){
 				
 				rnd = getElementFn();
 				
 			}else{
-				if( self.settings.randomize!==null && typeof self.settings.randomize==='function' ){
+				if(self.settings.randomize !== null && typeof self.settings.randomize === 'function'){
 					rnd = _getCustom();
 				}else if( self.settings.auto ){
 					rnd = _getNext();
@@ -367,7 +364,7 @@
 			
 			//Set current active element
 			_active.index = _getIndexFromOffset();
-			_active.el = $titles.get(_active.index);
+			_active.el = $tiles.get(_active.index);
 			
 			//Get random element offset and delay
 			var offset = _getOffset(rnd.index),
@@ -376,13 +373,13 @@
 			//Check direction to prevent jumping
 			if( rnd.index>_active.index ){
 				//We are moving to the prev (first to last)
-				if( _active.index===0 && rnd.index===$titles.length-1 ){
-					$container.css("margin-top", _getOffset($titles.length) );
+				if(_active.index === 0 && rnd.index === $tiles.length-1){
+					$container.css('margin-top', _getOffset($tiles.length) );
 				}
 			}else{
 				//We are moving to the next (last to first)
-				if( _active.index===$titles.length-1 && rnd.index===0 ){
-					$container.css("margin-top", 0 );
+				if( _active.index===$tiles.length-1 && rnd.index===0 ){
+					$container.css('margin-top', 0);
 				}
 			}
 			
@@ -392,13 +389,13 @@
 			//Perform animation
 			$container.animate({
 				marginTop : offset
-			}, delay, "easeOutBounce", function (){
+			}, delay, 'easeOutBounce', function (){
 				
-				_setAnimationFX("stop");
+				_setAnimationFX(FX_STOP);
 			
 				_isRunning = false;
 				
-				if( typeof _oncompleteShuffling==="function" ){
+				if(typeof _oncompleteShuffling === 'function'){
 							
 					_oncompleteShuffling($slot, _active);
 					
@@ -410,7 +407,7 @@
 			
 			//Change blur
 			setTimeout(function(){
-				_setAnimationFX("false", false);
+				_setAnimationFX('false', false);
 			}, delay/2);
 			
 		}
@@ -420,7 +417,7 @@
 		  * @return int - Returns true if machine is on the screen
 		*/
 		function _isVisible(){
-			if( self.settings.stopHidden===false ){
+			if(self.settings.stopHidden === false){
 				return false;
 			}
 			//Stop animation if element is [above||below] screen, best for performance
@@ -435,7 +432,7 @@
 		*/
 		function _auto(){
 			
-			if( _forceStop===false ){
+			if(_forceStop === false){
 				
 				_timer = setTimeout(function(){
 					
@@ -451,30 +448,36 @@
 		
 		$slot.css("overflow", "hidden");
 		
+		//Validate active index
+		if(this.settings.active < 0 || this.settings.active >= $tiles.length ){
+			this.settings.active = 0;
+			_active.index = 0;
+			_active.el = $tiles.get(0);
+		}
+		
 		//Wrap elements inside $container
-		$titles.wrapAll("<div class='slotMachineContainer' />");
+		$tiles.wrapAll("<div class='slotMachineContainer' />");
 		$container = $slot.find(".slotMachineContainer");
 		
 		//Set max top offset
-		_maxTop = -$container.height();
+		_maxTop = - $container.height();
 		
 		//Add the last element behind the first to prevent the jump effect
-		_$fakeFirstTitle = $titles.last().clone();
-		_$fakeLastTitle = $titles.first().clone();
+		_$fakeFirstTile = $tiles.last().clone();
+		_$fakeLastTile = $tiles.first().clone();
 		
-		$container.prepend( _$fakeFirstTitle );
-		$container.append( _$fakeLastTitle );
+		$container.prepend( _$fakeFirstTile );
+		$container.append( _$fakeLastTile );
 		
 		//Set min top offset
-		_minTop = -_$fakeFirstTitle.outerHeight();
+		_minTop = -_$fakeFirstTile.outerHeight();
 		
 		//Show active element
-		$container.css("margin-top", _getOffset(self.settings.active) );
+		$container.css('margin-top', _getOffset(self.settings.active));
 		
 		//Start auto animation
-		if( self.settings.auto!==false ){
-			
-			if( self.settings.auto===true ){
+		if(self.settings.auto !== false){
+			if(self.settings.auto === true){
 				_shuffle();
 			}else{
 				_auto();
@@ -513,8 +516,6 @@
 				if( self.settings.auto!==false && _timer!==null ){
 					clearTimeout(_timer);
 				}
-				
-				
 				
 				if( typeof repeations==='number' && repeations>0 ){
 					$container.clearQueue().stop(true, false);
@@ -570,7 +571,7 @@
 	function _getInstance(element, options){
 		var machine;
 		if ( !$.data(element, 'plugin_' + pluginName) ){
-			machine = new Plugin(element, options);
+			machine = new SlotMachine(element, options);
 			$.data(element, 'plugin_' + pluginName, machine);
 		}else{
 			machine = $.data(element, 'plugin_' + pluginName);
